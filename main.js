5f518599f678e550972c4d1d783c815d36a0190d
taiigarr <t6rodrig@ryerson.ca>
2017-06-02 08:57:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 jQuery(function($) {
-	$(".nav .nav-hamburger").click(function() {
-		
+	$(".nav-hamburger").click(function() {
+
 		$(".nav .nav-wrapper").toggleClass("nav-open");
 		
 	});
+	$(window).resize(function() {
+		if ($(".nav .nav-wrapper").hasClass("nav-open")) {
+			$(".nav-hamburger").click();
+		}
+	});
 });
+
