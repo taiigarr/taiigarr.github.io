a96c003a4288f43eae65a416531fae299aed38cc
taiigarr <t6rodrig@ryerson.ca>
2017-06-08 10:07:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-jQuery(function($) {
+jQuery(document).ready(function($) {
 	$(".nav-hamburger").click(function() {
 
 		$(".nav .nav-wrapper").toggleClass("nav-open");
 		
 	});
 	$(window).resize(function() {
-		if ($(".nav .nav-wrapper").hasClass("nav-open") && document.body.clientWidth > 900) {
+		if ($(".nav .nav-wrapper").hasClass("nav-open") && document.body.clientWidth > 800) {
 			$(".nav-hamburger").click();
 		}
 	});
+	setBindings();
+		
+	function setBindings() {
+		$(".nav").on('click', 'a', function (e) { 
+	 	e.preventDefault();
+			var sectionID = e.currentTarget.id + "-section";
+			$("html,body").animate({
+				scrollTop: $("." + sectionID).offset().top
+			},1000);
+		});
+	}
 });
 
